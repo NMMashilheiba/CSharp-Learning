d8cc314cbfa4fecea73f17affd5cadb75252dc1d
NMMashilheiba <gdragonnm2@gmail.com>
2025-06-28 22:24:44 +0530

    Handling Exception

---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+		try
+		{
+			string content = File.ReadAllText(@"C:\Users\mashi\OneDrive\Documents\NuclearCod.txt");
+			Console.WriteLine(content);
+		}
+		catch (FileNotFoundException exMsg )
+		{
+			Console.WriteLine("There was a problem.");
+			Console.WriteLine(exMsg.Message);
+		}
+		catch (DirectoryNotFoundException exMsg)
+		{
+			Console.WriteLine("Directory not found, please check and try again.");
+			Console.WriteLine(exMsg);
+		}
+		catch (Exception exMsg)
+		{
+			Console.WriteLine("There was a problem!");
+			Console.WriteLine(exMsg);
+		}
+		finally
+		{
+			Console.WriteLine("Finally block: Finally block is always executed.");
+		}
+
+		Console.ReadLine();
+    }
+}
